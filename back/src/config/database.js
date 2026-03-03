7813c80b0ade9da33bb3b29c4512dec22c5beeb7
kimds8036 <kimds8036@naver.com>
2026-03-03 20:29:10 +0900

    백엔드 깃 이그노어 적용 및 환경변수 수정 도커 적용용

--- back/src/config/database.js
+++ back/src/config/database.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'cucumber_db',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: Number(process.env.DB_PORT) || 3307,
+  user: process.env.DB_USER || 'cucumber',
+  password: process.env.DB_PASSWORD || 'cucumber0425',
+  database: process.env.DB_NAME || 'cucumber',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
