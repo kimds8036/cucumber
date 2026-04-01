1ab8b8146a44fcf7966d01807a0678b883283686
eunchae0215 <eunchae040215@naver.com>
2026-04-01 22:34:33 +0900

    사진 경고 해결

--- frontend/view/src/boardWrite.jsx
+++ frontend/view/src/boardWrite.jsx
@@ -122,7 +122,7 @@ const BoardWrite = ({ navigation, route }) => {
 
   const handlePickPostImages = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: 'images',
       allowsMultipleSelection: true,
       quality: 0.8,
       selectionLimit: 5,
